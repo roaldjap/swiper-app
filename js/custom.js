db0e5623b67c5b6b4f06b68e273d6ae3b87c6e1b
Ben Sebastian <asusx555ld@example.com>
2015-06-08 17:32:26 +0800

    update bg images

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,11 @@
 $(document).ready(function() {
   
-  $('.carousel').each(function(){
+  $('.carousel').each(function() {
     $(this).carousel();
 
     var carousel = $(this).data('bs.carousel'); // or .data('carousel') in bootstrap 2
     carousel.pause();
-
-    // At first, reverse the order of the items in the carousel because we're moving backwards
-    $(this).find('> .carousel-inner > .item:not(:first-child)').each(function() {
-      $(this).prependTo(this.parentNode);
-    });
-
-    // Override the bootstrap carousel prototype function, adding a different one won't work (it'll work only for the first slide)
-    carousel.cycle = function (e) {
-      console.log(this);
-      if (!e) this.paused = false
-      if (this.interval) clearInterval(this.interval);
-      this.options.interval
-        && !this.paused
-        && (this.interval = setInterval($.proxy(this.prev, this), this.options.interval))
-      return this;
-    };
-  });
-  
-  $(window).load(function() {
-    carousel.cycle();
+    
   });
   
   //Select All Checkboxes
@@ -54,6 +35,32 @@ $(document).ready(function() {
     }
   });
   
+});
+
+$(window).load(function() {
+  
+  $('.carousel').each(function() {
+    
+    var carousel = $(this).data('bs.carousel'); // or .data('carousel') in bootstrap 2
+    
+    // At first, reverse the order of the items in the carousel because we're moving backwards
+    $(this).find('> .carousel-inner > .item:not(:first-child)').each(function() {
+      $(this).prependTo(this.parentNode);
+    });
+    
+    // Override the bootstrap carousel prototype function, adding a different one won't work (it'll work only for the first slide)
+    carousel.cycle = function (e) {
+      if (!e) this.paused = false
+      if (this.interval) clearInterval(this.interval);
+      this.options.interval
+        && !this.paused
+        && (this.interval = setInterval($.proxy(this.prev, this), this.options.interval))
+      return this;
+    };
+
+    carousel.cycle();
+    
+  });
 });
 
 $('.carousel-control').click(function(e) {
