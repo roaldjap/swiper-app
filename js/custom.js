e31e4c05aacea482fe171d177b77a27854eb3da4
Ben Sebastian <asusx555ld@example.com>
2015-06-10 17:39:04 +0800

    fixed viewport change for home only

--- js/custom.js
+++ js/custom.js
@@ -100,28 +100,3 @@ $(window).scroll(function () {
   }
   
 });
-
-(function(){
-  var setViewportHeight = (function(){
-    function debounced(){
-      document.documentElement.style.height = window.innerHeight + "px";
-      if (document.body.scrollTop !== 0) {
-        window.scrollTo(0, 0);
-      }
-    }
-    var cancelable = null;
-
-    return function(){
-      cancelable && clearTimeout(cancelable);
-      cancelable = setTimeout(debounced, 100);
-    };
-  })();
-
-  //ipad safari
-  if(/iPad/.test(navigator.platform) && /Safari/i.test(navigator.userAgent)){  
-    window.addEventListener("resize", setViewportHeight, false);
-    window.addEventListener("scroll", setViewportHeight, false);
-    window.addEventListener("orientationchange", setViewportHeight, false);
-    setViewportHeight();
-  }
-})();
